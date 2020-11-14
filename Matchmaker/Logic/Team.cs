7f1c7da2dfb1f5e172f0764880a90d59bd3e7cf8
Philipp Hinz <phisn@outlook.de>
2020-11-14 01:30:06 +0100

    Added properbilty calculation for two teams

--- Matchmaker/Logic/Team.cs
+++ Matchmaker/Logic/Team.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Moserware.Skills;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +7,35 @@ namespace Matchmaker.Logic
 {
     public class Team
     {
+        public static double GetScoreFor(List<Participant> redPlayers, List<Participant> bluePlayers)
+        {
+            var redTeam = new Moserware.Skills.Team();
+
+            foreach (Participant participant in redPlayers)
+            {
+                redTeam.AddPlayer(
+                    new Player(participant.ParticipantId),
+                    new Rating(
+                        participant.Mean,
+                        participant.StandardDeviation));
+            }
+
+            var blueTeam = new Moserware.Skills.Team();
+
+            foreach (Participant participant in bluePlayers)
+            {
+                blueTeam.AddPlayer(
+                    new Player(participant.ParticipantId),
+                    new Rating(
+                        participant.Mean,
+                        participant.StandardDeviation));
+            }
+
+            return TrueSkillCalculator.CalculateMatchQuality(
+                GameInfo.DefaultGameInfo,
+                Moserware.Skills.Teams.Concat(blueTeam, redTeam));
+        }
+
         public List<int> Participants { get; set; } = new List<int>();
     }
 }
