6b1a2a7b0c526c13e07a40cda18b4e322a18e38f
Philipp Hinz <phisn@outlook.de>
2020-11-15 12:54:19 +0100

    added matchmakingquick for quick creation of teams

---
+++ Matchmaker/Actions/MatchmakingQuick.cs
@@ -0,0 +1,33 @@
+﻿using Matchmaker.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Matchmaker.Actions
+{
+    public class MatchmakingQuick : Action
+    {
+        public override string Name => "matchmaking quick";
+        public override string Description => "removes all current participants, adds new and shuffels";
+        public override string Usage => "matchmaking quick [participants...]";
+
+        public override bool Call(string[] arguments)
+        {
+            Teams.Blue.Participants.Clear();
+            Teams.Red.Participants.Clear();
+
+            using (Context context = new Context())
+            {
+                foreach (string argument in arguments)
+                {
+                    Participant participant = Participant.FindParticipantByString(argument);
+                    Teams.Blue.Participants.Add(participant.ParticipantId);
+                }
+            }
+
+            Teams.Optimize();
+            return true;
+        }
+    }
+}
