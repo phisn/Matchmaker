027ac9c98b2514b28a164c537e8286904c6da36a
Philipp Hinz <phisn@outlook.de>
2020-11-15 11:46:04 +0100

    updated method call name in matchmaker

--- Matchmaker/Actions/Matchmaking.cs
+++ Matchmaker/Actions/Matchmaking.cs
@@ -46,7 +46,7 @@ namespace Matchmaker.Actions
 
             allTeams = allTeams.OrderBy((t) =>
             {
-                return 1.0 - Team.GetScoreFor(t.Key, t.Value);
+                return 1.0 - Team.GetRatingFor(t.Key, t.Value);
 
             }).ToList();
 
